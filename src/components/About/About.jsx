502b10326912ae1ecc54eb08b8ea831f3b7fb51a
KrishJain45 <krishjain4683@gmail.com>
2025-08-25 03:22:05 +0530

    resume update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
          
           <a
-            href="https://docs.google.com/document/d/14PP0E1MiuvTT2TYvX3eMm7sAL8A6lrKw/edit?usp=drivesdk&ouid=106050798875296361385&rtpof=true&sd=true"
+            href="https://drive.google.com/file/d/14cDq4IWPO9M3O47itHKLseT_OPOj3OqS/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
